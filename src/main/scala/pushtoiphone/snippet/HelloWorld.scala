9e67ff87e76c41a550ba9f1683efda178658628a
Mahmood Ali <mahmood@notnoop.com>
2011-03-14 15:59:51 -0400

    typo fix

--- src/main/scala/pushtoiphone/snippet/HelloWorld.scala
+++ src/main/scala/pushtoiphone/snippet/HelloWorld.scala
@@ -5,7 +5,7 @@ import _root_.scala.xml.{NodeSeq, Text}
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
 import _root_.java.util.Date
-import code.lib._
+import com.judeapps.pushtoiphone.lib._
 import Helpers._
 
 class HelloWorld {
